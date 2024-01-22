916559ee88b54c674cc57419854a012c991610fc
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2024-01-22 15:43:35 +0000

    acceleration reimplemented
    turns put i didn't have acceleration implemented yet so I set up acceleration to work in the physics model class

--- Physics Framework/DX11PhysicsFramework/PhysicsModel.cpp
+++ Physics Framework/DX11PhysicsFramework/PhysicsModel.cpp
@@ -8,6 +8,7 @@ PhysicsModel::PhysicsModel(Transform* transform)
 void PhysicsModel::Update(float deltaTime)
 {
 	XMFLOAT3 position = _transfrom->GetPosition();
+	XMStoreFloat3(&_velocity, XMLoadFloat3(&_velocity) + XMLoadFloat3(&_Acceleration) * deltaTime);
 	XMStoreFloat3(&position, XMLoadFloat3(&position) + (XMLoadFloat3(&_velocity) * deltaTime));
 
 	_transfrom->SetPosition(position);
--- Physics Framework/DX11PhysicsFramework/PhysicsModel.h
+++ Physics Framework/DX11PhysicsFramework/PhysicsModel.h
@@ -7,12 +7,15 @@ protected:
 
 	Transform* _transfrom;
 	XMFLOAT3 _velocity = XMFLOAT3(0, 0, 0);
-	float _Acceleration = 0;
+	XMFLOAT3 _Acceleration = XMFLOAT3(0,0,0);
 public:
 	PhysicsModel(Transform* transform);
 	void Update(float deltaTime);
 	XMFLOAT3 GetVelocity() { return _velocity; }
 	void SetVelocity(XMFLOAT3 velocity) { _velocity = velocity; }
+
+	void SetAcceleration(XMFLOAT3 acceleration) { _Acceleration = acceleration; }
+	XMFLOAT3 GetAcceleration() { return _Acceleration; }
 
 };
 
